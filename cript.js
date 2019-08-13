e7dcc52e94e398759e72670b19c597c3fbff395f
yash chaudhary <yash2200@gmail.com>
2019-08-13 20:03:56 +0530

    Initial commit

---
+++ cript.js
@@ -0,0 +1,63 @@
+
+//CONVERTING AGE INTO NO OF DAYS
+
+// var age=prompt("what is your age ");
+// var days=365 * age;
+// alert("you are " + days + " years old")
+// console.log("you are " + days + " years old");
+
+//GUESSING A NO
+
+// var no=7; 
+// while(guess!=7){
+// var guess = prompt("guess a no");
+// if(guess==7)
+//     alert("you got it right");
+// else
+//     alert("try again");    
+// }
+
+//ANNOY-OMATIC
+
+// var ans = prompt("are we there yet");
+// while(ans != "no" && ans != "yeah")
+// {
+//     var ans = prompt("are we there yet");
+        
+// }
+// alert("yay, we finally made it");
+
+//DOM MANUPLATION
+
+// var lis= document.querySelector("ul");
+
+// for(i=0;i<lis.length;i++)
+// {
+//     lis[i].addEventListener("click",function(){
+//         this.style.background= "pink";
+//         this.style.color= "blue";
+
+//     })
+
+// }
+
+//LET THE GAME BEGINS
+
+var button1=document.querySelector("#p1");
+var button2=document.querySelector("#p2");
+var p1_display=document.querySelector("#p-1");
+var p2_display=document.querySelector("#p-2");
+var p1_score=0;
+var p2_score=0;
+
+button1.addEventListener("click",function(){
+    alert("asdnkcasd asv");
+    // p1_score+=1;
+    // p1_display.textContent=p1_score;
+
+});
+button2.addEventListener("click",function(){
+    // p2_score += 1;
+    // p2_display.textContent = p2_score;
+
+});
